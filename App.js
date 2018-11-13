f8819460ea62a3e3e41e44d0e6e11943dd742560
Riley Lundquist <rlundquist3@gmail.com>
2018-11-13 18:33:07 +0000

    readme

--- App.js
+++ App.js
@@ -21,7 +21,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native, asshole!</Text>
+        <Text style={styles.welcome}>Welcome to React Native, dickbucket!</Text>
+        <Text>Sorry. That was rude.</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
